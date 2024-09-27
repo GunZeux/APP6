6d85f043adca3b878cff168f21ee9575c3a233cb
GunZ_Lap2 <gunzeux71@gmail.com>
2024-09-27 09:26:00 +0530

    Made All links and added table to the home page

--- main.py
+++ main.py
@@ -4,11 +4,15 @@ import pandas as pd
 
 app = Flask(__name__)
 
+station_file = "data/stations.txt"
+stations = pd.read_csv(station_file, skiprows=17)
+stations = stations[["STAID", "STANAME                                 "]]
+
 
 @app.route("/")
 @app.route("/home")
 def domain():
-    return render_template("home.html")
+    return render_template("home.html", data=stations.to_html())
 
 
 @app.route("/api/v1/<station>/<date>")
@@ -19,6 +23,24 @@ def about(station, date):
     return {"station": station,
             "date": date,
             "temperature": temperature}
+
+
+@app.route("/api/v1/yearly/<station>/<year>")
+def yearly(station, year):
+    filename = "data/TG_STAID" + str(station).zfill(6) + ".txt"
+    df = pd.read_csv(filename, skiprows=20)
+    df["    DATE"] = df["    DATE"].astype(str)
+    temperature = df.loc[df["    DATE"].str.startswith(str(year))]
+    result = temperature.to_dict(orient="records")
+    return result
+
+
+@app.route("/api/v1/<station>")
+def a_station(station):
+    filename = "data/TG_STAID" + str(station).zfill(6) + ".txt"
+    df = pd.read_csv(filename, skiprows=20)
+    result = df.to_dict(orient="records")
+    return result
 
 
 if __name__ == "__main__":
